19d8478a563a2f38c4e7393ca96421491abd7550
apalominoch <apalominochesterton@gmail.com>
2025-02-08 15:01:52 +0000

    Added a file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import pandas as pd
+st.title('IM Calculator')
+st.divider()
+st.subheader('Current Position:')         
+data = pd.read_csv('POSITION - 20250103.csv')
+st.dataframe(data)
+st.subheader('Add new trades:')
+new_trades = pd.DataFrame(columns=data.columns)
+edited = st.data_editor(new_trades,num_rows='dynamic')
+
+st.write('here is where the calcs would appear but not pasting my code here')
